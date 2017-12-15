f2a4f74c9b18123fd97eb10a64697a3be66b14ff
Leif Swenson <leifswenson@gmail.com>
2017-12-15 23:14:47 +0000

    made questions an array in js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,14 @@
+// Hide  elements
+//=======================================================================================================================
+   //$('.questions').hide();
+   //$('.game-results').hide();
+
+
+
+
 $(document).ready(function() {
-    
+  
+
 
 // Variables
 //=======================================================================================================================
@@ -8,7 +17,62 @@ $(document).ready(function() {
 var numCorrect;
 var numIncorrect;
 var numUnanswered;
+var counter;
 
+// Game questions and answers
+
+var questions = [{
+  
+    question: "Who has the most NBA championships as a player?",
+    options: ["Michael Jordan", "Bill Russell", "Robert Horry", "Magic Johnson"],
+    answer: "Bill Russell"
+    
+  }, {
+    question: "Who has the most career NFL passing yards?",
+    options: ["Peyton Manning", "Brett Favre", "Drew Brees", "Dan Marino"],
+    answer: "Peyton Manning"
+    
+  }, {
+    
+    question: "What golfer has the most career PGA Tour wins?",
+    options: ["Jack Nicklaus", "Tiger Woods", "Sam Snead", "Arnold Palmer"],
+    answer: "Sam Snead"
+    
+  }, {
+    
+    question: "What school has the most NCAA division I men's basketball championships?",
+    options: ["North Carolina", "Duke", "UCLA", "Kentucky"],
+    answer: "UCLA"
+    
+  }, {
+    
+    question: "What player has the most World Series rings?",
+    options: ["Yogi Berra", "Joe DiMaggio", "Lou Gehrig", "Phil Rizzuto"],
+    answer: "Yogi Berra"
+    
+  }, {
+    
+    question: "What golfer has the most wins in Major Championships?",
+    options: ["Walter Hagen", "Tiger Woods", "Ben Hogan", "Jack Nicklaus"],
+    answer: "Jack Nicklaus"
+    
+  }
+];
+
+
+// Loop over questions to display them on the page with the answer options
+
+for(var i=0; i<questions.length; i++) {
+  $(".question").append(questions[i].question);
+  
+  for (var j=0; j<questions[i].options.length; j++) {
+    $(".options").append(questions[i].options[j]);
+  }
+  
+  console.log(questions[i].question);
+  console.log(questions[i].options);
+  console.log(questions[i].answer);
+}
 
 
 
@@ -36,41 +100,51 @@ function newGame() {
 
  
   $("#correct-answer").text(numCorrect);
+  $("#incorrect-answer").text(numIncorrect);
+  $("#unanswered").text(numUnanswered);
     
     
     
-    
-    
+   
+  // Timer ========================
+  
     //  Set our counter to 120secs
-    var number = 120;
+    counter = 120;
 
-    //  Variable that will hold our interval ID when we hit run
+    //  Variable that will hold our interval ID when we hit start
     var intervalId;
 
     //  When the start button gets clicked, execute the run function.
     $(".btn-primary").on("click", run);
 
-    //  The run function sets an interval
-    //  that runs the decrement function once a second.
+    
+    //  The run function sets an interval that runs the decrement function once a second.
     function run() {
       intervalId = setInterval(decrement, 1000);
+      $('.questions').show();
     }
 
     //  The decrement function.
     function decrement() {
+      
+      //  Decrease counter by one.
+      counter--;
 
-      //  Decrease number by one.
-      number--;
-
-      //  Show the number in the .display-time tag.
-      $(".display-time").html("<h2>" + number + "</h2>");
+      //  Show the counter in the .display-time tag.
+      $(".display-time").html("<h2>" + counter + "</h2>");
 
 
-      //  Once number hits zero...
-      if (number === 0) {
+      //  Once counter hits zero...
+      if (counter === 0) {
 
         //  ...run the stop function.
         stop();
+        
+        // hide all the questions
+        // $('.questions').hide();
+        
+        // show the results
+        // $('.game-results').show();
 
         //  Alert the user that time is up.
         alert("Time Up!");
