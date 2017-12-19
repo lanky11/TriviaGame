db18d6dbcf39ee3c39afa56df368b43b13b75977
Leif Swenson <leifswenson@gmail.com>
2017-12-19 16:12:51 +0000

    finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,8 @@
 
   
-
-$(".start-game").on("click", function() {
-  game.start();
-}); 
-
-
-$(document).on("click", "#end-game", function(){
-  game.done();
-});
-
-
 // Game questions and answers
+// =========================================================================================
+
 var questions = [{
   
     question: "Who has the most NBA championships as a player?",
@@ -51,31 +42,39 @@ var questions = [{
 ];
 
 
+// Variables
+// =========================================================================================
 
 
-// create object that contains functions and variables needed for the game
-var game = {
-  correct: 0,
-  incorrect: 0,
-  counter: 10,
+  var correct = 0;
+  var incorrect = 0;
+  var counter = 60;
   
-  countdown: function() {
-	  game.counter--;
-	  $("#counter").html(game.counter);
+  
+// Functions
+// =========================================================================================
 
-	  if (game.counter <= 0) {
+
+// Functions to count down from 60
+  function countdown() {
+	  counter--;
+	  $("#counter").html(counter);
+
+	  if (counter <= 0) {
 	    // run done function when time reaches 0
-		  game.done();
+		  done();
 	  }
-	},
+	};
 	
-	start: function() {
+//Function that gets called when the start button is clicked	
+	 function start() {
 	  
 	  // sets timer to run countdown function ever 1sec
-	  timer = setInterval(game.countdown, 1000);
+	  timer = setInterval(countdown, 1000);
 	  $('.main-display').prepend('<h2>Time Remaining: <span id="counter">60</span> Seconds</h2>');
 	  
-	  // remove start game button
+	  // remove start game button and instructions
+	  $('.instructions').remove()
 	  $('.start-game').remove();
   
     // Loop over questions to display them on the page with the answer options
@@ -83,93 +82,101 @@ var game = {
       $(".main-display").append('<h2>' + questions[i].question + '</h2>');
       
       for (var j=0; j<questions[i].options.length; j++) {
-        $(".main-display").append('<input type="radio" name ="question' + '-' + i + '"value="' + questions[i].options[j] + '">' + questions[i].options[j]);
+        $(".main-display").append('<br><input type="radio" name ="question' + '-' + i + '"value="' + questions[i].options[j] + '">' + questions[i].options[j]);
       }
     }
     
-    $('.main-display').append('<br><button id="end-game">End Game</button>')
+    $('.main-display').append('<br><button class="btn btn-primary btn-lg m-3" id="end-game">End Game</button>')
   	  
-  },
+  };
   
   
-  
-  done: function() {
+// Function that will run when the end game button is clicked. 
+   function done() {
     
     //checks clicked answer against answer for questions 1-6
     $.each($("input[name='question-0']:checked"), function() {
   			if ($(this).val() == questions[0].answer) {
-  				game.correct++;
+  				correct++;
   			} else {
-  				game.incorrect++;
+  				incorrect++;
   			}
 
   		});
   		
   	$.each($("input[name='question-1']:checked"), function() {
   			if ($(this).val() == questions[1].answer) {
-  				game.correct++;
+  				correct++;
   			} else {
-  				game.incorrect++;
+  				incorrect++;
   			}
 
   		});
   		
   	$.each($("input[name='question-2']:checked"), function() {
   			if ($(this).val() == questions[2].answer) {
-  				game.correct++;
+  				correct++;
   			} else {
-  				game.incorrect++;
+  				incorrect++;
   			}
 
   		});
   		
   	$.each($("input[name='question-3']:checked"), function() {
   			if ($(this).val() == questions[3].answer) {
-  				game.correct++;
+  				correct++;
   			} else {
-  				game.incorrect++;
+  				incorrect++;
   			}
 
   		});
   		
   	$.each($("input[name='question-4']:checked"), function() {
   			if ($(this).val() == questions[4].answer) {
-  				game.correct++;
+  				correct++;
   			} else {
-  				game.incorrect++;
+  				incorrect++;
   			}
 
   		});
   		
   	$.each($("input[name='question-5']:checked"), function() {
   			if ($(this).val() == questions[5].answer) {
-  				game.correct++;
+  				correct++;
   			} else {
-  				game.incorrect++;
+  				incorrect++;
   			}
 
   		});
   
   // shows the result of quiz		
-  this.result();
-    
-  },
+  result();
   
-  result: function() {
+    
+  }
+  
+// Renders the info to the page.  
+   function result() {
   	clearInterval(timer);
   
     $(".main-display h2").remove();
     $(".main-display").html("<h2>You're Done!</h2>");
-    $(".main-display").append("<h3>Correct: " + this.correct + "</h3>");
-    $(".main-display").append("<h3>Incorrect: " + this.incorrect + "</h3>");
-    $(".main-display").append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+    $(".main-display").append("<h3>Correct: " + correct + "</h3>");
+    $(".main-display").append("<h3>Incorrect: " + incorrect + "</h3>");
+    $(".main-display").append("<h3>Unanswered: " + (questions.length - (incorrect + correct)) + "</h3>");
   
   }
-  
-	
-};
+
   
     
-    
+// On click events for starting and ending a game.
+// =========================================================================================
 
-
+  $(".start-game").on("click", function() {
+    start();
+  }); 
+  
+  
+  $(document).on("click", "#end-game", function(){
+    done();
+  });
